0d9b5a90517c2bafe3c9086afbafd5952cdf87b3
Luis <dev.luissm@gmail.com>
2022-03-16 18:44:07 -0600

    add : Practica inicial Pokeapi

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Repo for PokeApi-JS 🐳
+## LaunchX : Mision-Frontend 💻 🚀
+
+PokeApi will be used to obtain the main characteristics of a Pokémon, random or chosen by the user.
+Design inspiration by [Ricardo Schiniegoski](https://dribbble.com/shots/17616780-Pok-dex).
+
+<sub>This page is beeing developed.<sub>
---
+++ pokeapi.js
@@ -0,0 +1,26 @@
+//Cambiarlo por eventlistener 
+
+const fecthPokemon = () => {
+    const pokeName = document.getElementById("pokeName");
+    let pokeInput = pokeName.value.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
+    fetch(url).then((res) =>{
+        if(res.status != "200"){
+            console.log(res);
+            pokeImage("./img/sad-pikachu.gif");
+        }
+        else{
+            return res.json();
+        }
+    }).then((data) => {
+        // console.log(data);
+        let pokeImg = data.sprites.front_default;
+        console.log(pokeImg);
+        pokeImage(pokeImg)
+    })
+}
+
+const pokeImage = (url) => {
+    const pokeImg = document.getElementById("pokeImg");
+    pokeImg.src = url;
+}
